460341debab26b856c202a3eebc075caa5807c06
Pierre-antoine Potage <pierre-antoine.potage@raccourci.fr>
2023-07-20 15:12:50 +0200

    feat : add first testÃ

---
+++ src/test/java/service/CrudTest.java
@@ -0,0 +1,15 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.openclassrooms.safetinet.model.Person;
+
+@SpringBootTest
+public class CrudTest {
+	
+	@Test
+	public Person getAllPerson{
+		 
+	}
+}
